85beeeeda3839c38105bdd71afa658d09a574642
Qmr2002 <asondos867@gmai.com>
2023-07-13 19:10:07 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
-#include <stdio.h> 
-int main(void){
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Succcess)
+ */
+
+ int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0); 
 } 
