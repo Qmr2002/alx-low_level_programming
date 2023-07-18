813776d4aeca0efa6c37f2a12c38cb9337463762
Qmr2002 <asondos867@gmai.com>
2023-07-18 16:20:48 -0700

    a

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+int _putchar(char c); 
 
-#include <stdio.h>
+void print_alphabet(void); 
 
-int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet_x10(void); 
 
+int _islower(int c); 
 
-#endif
+int _isalpha(int c); 
+
+int print_sign (int n); 
+
+int_abs(int); 
+
+int print_last_digit(int); 
+
+void jack_bauver(void); 
+
+void times_table(void); 
+
+int add(int, int); 
+
+void print_to_98(int n); 
+
+void print_times_table(int n); 
