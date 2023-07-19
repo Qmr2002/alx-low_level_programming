82efc3671bc2af011f6f82307c7c4e63d4ee0d3d
Qmr2002 <asondos867@gmai.com>
2023-07-18 19:23:32 -0700

    si

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
+#include "main.h"
+
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char school[10] = "_putchar";
 
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
 	return (0);
 }
